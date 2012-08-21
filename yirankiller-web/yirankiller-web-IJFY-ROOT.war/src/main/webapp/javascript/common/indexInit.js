1b1f6410f755040797798152a505bfd40b080684
yirankiller <yirankiller@gmail.com>
2012-08-21 18:38:42 -0400

    add indexInit.js

---
+++ yirankiller-web/yirankiller-web-IJFY-ROOT.war/src/main/webapp/javascript/common/indexInit.js
@@ -0,0 +1,10 @@
+/**
+ * User: easliu
+ * Date: 8/21/12
+ * Version: 1.0
+ */
+define(function(require,exports,module){
+    var $ = require("jQuery");
+    require('jQueryUI.tabs')($);
+    exports
+});
