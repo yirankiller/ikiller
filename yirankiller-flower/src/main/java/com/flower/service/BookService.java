5b2c86b0c5d8bcefe35e451309b0a81139400ef9
yirankiller <yirankiller@gmail.com>
2014-05-07 23:59:17 +0800

    backup for ..

--- yirankiller-flower/src/main/java/com/flower/service/BookService.java
+++ yirankiller-flower/src/main/java/com/flower/service/BookService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class BookService {
     @Autowired
     private BookDao bookDao;
