a0fbf77662b26e4ba0cf7481562944d955d8880e
easliu <eason.liu@leandev.se>
2014-10-24 18:45:32 +0800

    spring security

---
+++ yirankiller-flower/src/main/java/com/flower/springframework/security/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.flower.springframework.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by eason on 14-10-24.
+ */
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
